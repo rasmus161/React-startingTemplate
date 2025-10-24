ce603441dc41218388f058a9997ab439f88e6827
Rasmus Porthin <Porthinr@arcada.fi>
2025-10-24 12:44:08 +0300

    convert footer and header from jsx to tsx

--- src/componets/Ui/Footer/Footer.tsx
+++ src/componets/Ui/Footer/Footer.tsx
@@ -1,4 +1,11 @@
-export function Footer({ children, className = "" }) {
+import type { ReactNode } from "react";
+
+type FooterProps = {
+  children: ReactNode;
+  className: string;
+};
+
+export function Footer({ children, className = "" }: FooterProps) {
   return (
     <footer
       className={`mt-auto flex min-h-[150px] items-center justify-center text-center ${className} `}
--- src/componets/Ui/header/header.tsx
+++ src/componets/Ui/header/header.tsx
@@ -3,7 +3,21 @@ import { Link, NavLink } from "react-router-dom";
 // Usage
 // For: generates a logo with a link
 // Takes in: -link- ( link = (react-router-path)) to link to on click and -logoSrc- (link to logo) to display + (styles) -size- (logo w and h) + -name- text for hover box
-export function LogoLink({ link, logoSrc, className = " ", size = 32, name }) {
+type LogoLinkProps = {
+  link: string;
+  logoSrc: string;
+  className: string;
+  size?: number;
+  name?: string;
+};
+
+export function LogoLink({
+  link,
+  logoSrc,
+  className = " ",
+  size = 32,
+  name,
+}: LogoLinkProps) {
   return (
     <div
       className={` ${className} logo-container group relative p-5 hover:opacity-80`}
@@ -19,7 +33,7 @@ export function LogoLink({ link, logoSrc, className = " ", size = 32, name }) {
 // Usage
 // For: showing a box on hover with text
 // takes in: -text- (text to display) + you ned to add | group relative | to the parent element for this to work
-export function HoverBox({ text }) {
+export function HoverBox({ text }: { text: string }) {
   return (
     <div className="pointer-events-none absolute top-full left-1/2 mt-2 w-max -translate-x-1/2 bg-white p-2 text-xs text-gray-700 opacity-0 shadow-lg transition-opacity group-hover:opacity-100">
       {text}
@@ -30,7 +44,12 @@ export function HoverBox({ text }) {
 // Usage
 //For: generates a nav with a list of links + (styled)
 //Takes in: navLInks (a array of objects (in format of = { id: 0, name: "", linkTo: "" }, )
-function HeaderNav({ navLinks }) {
+type navLink = { id: number; name: string; linkTo: string };
+type HeaderNavProps = {
+  navLinks: navLink[];
+};
+
+export function HeaderNav({ navLinks }: HeaderNavProps) {
   return (
     <nav className="ml-auto bg-red-200 pr-10">
       <ul className="flex gap-2 bg-amber-300">
@@ -56,7 +75,7 @@ function HeaderNav({ navLinks }) {
 }
 
 export function Header() {
-  const navLinks = [
+  const navLinks: navLink[] = [
     { id: 0, name: "Home", linkTo: "/" },
     { id: 1, name: "example", linkTo: "/example" },
   ];
